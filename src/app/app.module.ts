c8625731026371fd9db804609a8d88083186146d
JJ Yeh <spyro12393@gmail.com>
2019-04-22 09:38:33 +0800

    <feat>: Start developing stridenav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { STRIDENavComponent } from './stridenav/stridenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    STRIDENavComponent
   ],
   imports: [
     BrowserModule
